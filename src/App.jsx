ca0f75240d41fb3f974bd05d5d0a57a7d2c13040
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-02-05 21:18:17 +0600

    empty react project created using vite, react-icons & tailwind css added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App container">
+      <p className="underline">hello from appointment manager web application!</p>
+    </div>
+  )
+}
+
+export default App
